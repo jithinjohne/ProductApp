fa2af1d087b023af5dd1c1ecc641e671a89f2c05
SAUMYA RAJAN <sarasaumya345@gmail.com>
2022-06-04 11:20:01 +0530

    Completed test

--- PrductApp.Web/Models/Product.cs
+++ PrductApp.Web/Models/Product.cs
@@ -9,7 +9,7 @@ namespace ProductApp.Web.Models
         public string Name { get; set; }
         [Required]
         public decimal? Price { get; set; }
-        [Required]
+        [Required][Range(minimum:0,maximum:double.MaxValue,ErrorMessage = "Value should be greater than 0")]
         public int? Stock { get; set; }
         [Required]
         public string Color { get; set; }
