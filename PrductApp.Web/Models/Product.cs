ad8011fb711ab9a343c1636431730c94898390e2
223296 <krishnapriya.ag@ust.com>
2022-08-02 13:16:49 +0400

    Third Change

--- PrductApp.Web/Models/Product.cs
+++ PrductApp.Web/Models/Product.cs
@@ -10,6 +10,7 @@ namespace ProductApp.Web.Models
         [Required]
         public decimal? Price { get; set; }
         [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock should be zero or a positive value")]
         public int? Stock { get; set; }
         [Required]
         public string Color { get; set; }
